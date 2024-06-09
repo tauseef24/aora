28f5de97e91d46229146db737c30b5b513a269c5
tauseef24 <73964188+tauseef24@users.noreply.github.com>
2024-06-09 23:04:05 +0530

    First Commit

---
+++ app/(auth)/sign-in.jsx
@@ -0,0 +1,94 @@
+import { View, Text, ScrollView, Image, Alert } from "react-native";
+import React, { useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "../../constants";
+import FormField from "../../components/FormField";
+import CustomButton from "../../components/CustomButton";
+import { Link, router } from "expo-router";
+import { signIn } from "../../lib/appwrite";
+import { useGlobalContext } from "../../context/GlobalProvider";
+
+const SignIn = () => {
+  const { setUser, setIsLoggedIn } = useGlobalContext();
+
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const submit = async () => {
+    if (!form.email || !form.password) {
+      Alert.alert("Error", "Please fill in all the fields");
+    }
+
+    setIsSubmitting(true);
+
+    try {
+      const result = await signIn(form.email, form.password);
+      setUser(result);
+      setIsLoggedIn(true);
+      // set it to global state
+      Alert.alert("Success", "User Signed in Successfully");
+      router.replace("/home");
+    } catch (error) {
+      Alert.alert("Error", error.message);
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
+
+  return (
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView>
+        <View className="w-full justify-center min-h-[85vh] px-4 my-6">
+          <Image
+            source={images.logo}
+            resizeMode="contain"
+            className="w-[115px] h-[35px]"
+          />
+
+          <Text className="text-2xl text-white mt-10 font-psemibold text-semibold">
+            Log In to Aora
+          </Text>
+
+          <FormField
+            title="Email"
+            value={form.email}
+            handleChangeText={(e) => setForm({ ...form, email: e })}
+            otherStyles="mt-7"
+            keyboardType="email-address"
+          />
+
+          <FormField
+            title="Password"
+            value={form.password}
+            handleChangeText={(e) => setForm({ ...form, password: e })}
+            otherStyles="mt-7"
+          />
+
+          <CustomButton
+            title="Sign In"
+            handlePress={submit}
+            containerStyles="mt-7"
+            isLoading={isSubmitting}
+          />
+
+          <View className="justify-center pt-5 flex-row gap-2">
+            <Text className="text-lg text-gray-100 font-pregular">
+              Don't Have Account?
+            </Text>
+            <Link
+              href="/sign-up"
+              className="text-lg font-psemibold text-secondary"
+            >
+              Sign Up
+            </Link>
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default SignIn;
---
+++ components/EmptyState.jsx
@@ -0,0 +1,25 @@
+import { View, Text, Image } from "react-native";
+import React from "react";
+import CustomButton from "./CustomButton"
+
+import { images } from "../constants";
+import { router } from "expo-router";
+
+const EmptyState = ({title, subtitle}) => {
+  return (
+    <View className="justify-center items-center px-4">
+      <Image
+        source={images.empty}
+        className="w-[270px] h-[215px]"
+        resizeMode="contain"
+      />
+
+      <Text className="text-xl text-center font-psemibold text-white mt-2">{title}</Text>
+      <Text className="font-pmedium text-sm text-gray-100">{subtitle}</Text>
+
+      <CustomButton title="Create Video" handlePress={() => router.push("/create")} containerStyles="w-full my-5" />
+    </View>
+  );
+};
+
+export default EmptyState;
